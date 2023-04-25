c23e31b9864e57a50f4bd2a6f53f36ede74b8724
Zhenbang Wei <znbang@gmail.com>
2023-04-25 18:07:08 +0800

    Using the Composition API

--- frontend/src/components/ZPagination.vue
+++ frontend/src/components/ZPagination.vue
@@ -2,35 +2,25 @@
   <q-pagination v-model="page" :max="total" @update:model-value="onUpdatePage" input size="1.5rem" class="justify-center" />
 </template>
 
-<script>
+<script setup>
 import { ref, watch } from 'vue'
 import { onBeforeRouteLeave, useRoute, useRouter } from 'vue-router'
 
-export default {
-  name: "ZPagination",
-  props: {
-    total: {
-      type: Number,
-      required: true,
-    }
-  },
-  setup(props) {
-    const $route = useRoute()
-    const $router = useRouter()
-    const page = ref(parseInt($route.params.page || $route.query.page || 1))
-
-    function onUpdatePage(page) {
-      $router.push($route.params.page ? {params: {...$route.params, page}} : {query: {...$route.query, page}})
-    }
-
-    onBeforeRouteLeave(watch(() => $route.query, () => {
-      page.value = parseInt($route.params.page || $route.query.page || 1)
-    }))
-
-    return {
-      page,
-      onUpdatePage,
-    }
+const $route = useRoute()
+const $router = useRouter()
+const page = ref(parseInt($route.params.page || $route.query.page || 1))
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
   }
+})
+
+function onUpdatePage(page) {
+  $router.push($route.params.page ? {params: {...$route.params, page}} : {query: {...$route.query, page}})
 }
+
+onBeforeRouteLeave(watch(() => $route.query, () => {
+  page.value = parseInt($route.params.page || $route.query.page || 1)
+}))
 </script>
